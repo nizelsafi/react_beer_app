57cab51ba39e9e486618162b7ab363ab1100930a
nizelsafi <nizelsafi@gmail.com>
2022-08-03 12:50:29 +0200

    add BeerItem style

--- src/components/beerItem.jsx
+++ src/components/beerItem.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
+import './beerItem.css';
 
 const BeerItem = (beerInfos) => {
-    const { image_url, name } = beerInfos.beerInfos;
+    const { id, image_url, name } = beerInfos.beerInfos;
     console.log(beerInfos);
     return (
-        <ImageListItem key={image_url}>
+        <ImageListItem key={id} className='listItem' >
           <img
             src={`${image_url}?w=248&fit=crop&auto=format`}
             srcSet={`${image_url}?w=248&fit=crop&auto=format&dpr=2 2x`}
             alt={name}
             loading="lazy"
+            className='imgItem'
           />
           <ImageListItemBar
             title={name}
