215669ba0f950491f9efa2fcf04f7c8430f9bdf3
nizelsafi <nizelsafi@gmail.com>
2022-08-05 18:21:01 +0200

    code refactoring

--- src/pages/beerCart.jsx
+++ src/pages/beerCart.jsx
@@ -1,11 +1,9 @@
-import {
-    Avatar,
-    List,
-    ListItem,
-    ListItemAvatar,
-    ListItemButton,
-    ListItemText
-} from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
