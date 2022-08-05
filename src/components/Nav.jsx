49306e2a6c2a399a0ef4ae9ac578943df989937a
nizelsafi <nizelsafi@gmail.com>
2022-08-05 13:49:32 +0200

    add a nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Nav.scss'
+import LocalGroceryStoreOutlinedIcon from '@mui/icons-material/LocalGroceryStoreOutlined';
+import Badge from '@mui/material/Badge';
+
+const Nav = ({ numOrderBeers }) => {
+    return (
+        <div className="nav">
+            <Link to="/">
+                <h3>HomePage</h3>
+            </Link>
+            <Link to="/beerList">
+                <h3>BeerList</h3>
+            </Link>
+            <Link to="/shopList" className='icon'>
+                { numOrderBeers > 0 ? 
+                    <Badge badgeContent={numOrderBeers} color="success">
+                        <LocalGroceryStoreOutlinedIcon />
+                    </Badge> :
+                    <LocalGroceryStoreOutlinedIcon />
+                }
+            </Link>
+        </div>
+    )
+}
+
+export default Nav
